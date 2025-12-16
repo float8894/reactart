544f6439b9ca4b98cb55c9b3fdf658f9a1f05d5d
Abhishek Panchal <abhishekpanchal8894@gmail.com>
2025-12-16 23:01:45 +0530

    add dynamic classes

--- src/components/AuthInputs.jsx
+++ src/components/AuthInputs.jsx
@@ -21,20 +21,22 @@ export default function AuthInputs() {
   const passwordNotValid = submitted && enteredPassword.trim().length < 6;
 
   return (
-    <div id="auth-inputs">
-      <div className="controls">
+    <div id='auth-inputs'>
+      <div className='controls'>
         <p>
-          <label>Email</label>
+          <label className={emailNotValid ? 'invalid' : undefined}>Email</label>
           <input
-            type="email"
+            type='email'
             className={emailNotValid ? 'invalid' : undefined}
             onChange={(event) => handleInputChange('email', event.target.value)}
           />
         </p>
         <p>
-          <label>Password</label>
+          <label className={emailNotValid ? 'invalid' : undefined}>
+            Password
+          </label>
           <input
-            type="password"
+            type='password'
             className={passwordNotValid ? 'invalid' : undefined}
             onChange={(event) =>
               handleInputChange('password', event.target.value)
@@ -42,11 +44,13 @@ export default function AuthInputs() {
           />
         </p>
       </div>
-      <div className="actions">
-        <button type="button" className="text-button">
+      <div className='actions'>
+        <button type='button' className='text-button'>
           Create a new account
         </button>
-        <button className='button' onClick={handleLogin}>Sign In</button>
+        <button className='button' onClick={handleLogin}>
+          Sign In
+        </button>
       </div>
     </div>
   );
